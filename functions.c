fc11a3dfcea561fcdca5e99c10e0abd6730bee8d
Llala <rene.raboshakga@gmail.com>
2023-09-18 18:10:48 +0200

    Task 0  and 1 function prototypes for the _printf function

---
+++ functions.c
@@ -0,0 +1,77 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_char - function that prints a character
+ * @c: character to print
+ * Return: printed character
+ */
+int print_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - function that prints a string
+ * @s: pointer to string to print
+ * Return: printed string
+ */
+int print_string(char *s)
+{
+	int len = 0;
+
+	if (s == NULL)
+		return (-1);
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (write(1, (s – len), len));
+}
+
+/**
+ * print_num - function that prints a number
+ * @d: number to print
+ *
+ * Return: printed number
+ */
+int print_num(int d)
+{
+	int l = 0, rev = 0, rev;
+	char positive[11];
+	char tmp;
+
+	if (d == 0)
+	{
+		positive[l] = ‘0’;
+		l++;
+	}
+	else
+	{
+		if (d < 0)
+		{
+			positive[l] = '-';
+			l++;
+			d = -d;
+		}
+		while (d > 0)
+		{
+			positive[l] = (d % 10) + ‘0’;
+			d /= 10;
+			l++;
+		}
+	}
+	positive[l] = '\0';
+	ver = l - 1;
+
+	while (rev < ver)
+	{
+		tmp = positive[rev];
+		positive[rev] = positive[ver];
+		positive[ver] = tmp;
+		rev++;
+		ver--;
+	}
+	return (write(1, positive, l));
+}
